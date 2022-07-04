800c1cd8efda5333a42d1f68b0740d3c8abad6b8
hiennm <hiennm11599@gmail.com>
2022-07-04 14:02:41 +0700

    update income

---
+++ incomeexpenseapi/income/serializers.py
@@ -0,0 +1,9 @@
+from dataclasses import fields
+from rest_framework import serializers
+from .models import Income
+
+class IncomeSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model=Income
+        fields=['id','date','description','amount','source']
--- incomeexpenseapi/incomeexpenseapi/settings.py
+++ incomeexpenseapi/incomeexpenseapi/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'authentication',
     'drf_yasg',
     'expenses',
+    'income'
 ]
 
 SWAGGER_SETTINGS={
